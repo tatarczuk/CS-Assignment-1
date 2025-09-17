c88cce53d8bc3378f0fb0404288a9fe9a8813b26
tatarczuk <tatarczuk@unbc.ca>
2025-09-17 16:31:03 -0700

    task 3

--- src/GroceryReceipt.java
+++ src/GroceryReceipt.java
@@ -19,13 +19,39 @@ public class GroceryReceipt {
         System.out.println("Enter the price of item 3: ");
         price3 = input.nextDouble();
         System.out.println("Are you a member? (y/n): ");
+        input.nextLine();
         isMember = input.nextLine();
+        System.out.println(isMember);
 
         double subtotal = price1 + price2 + price3;
         double discount = 0;
-        if(isMember == "y") {
+        if(isMember.equals("y")) {
             discount = -LOYALTY_DISCOUNT * subtotal;
         }
+        double taxableAmount = subtotal + discount;
+        double gst = taxableAmount * GST_RATE;
+        double pst = taxableAmount * PST_RATE;
+        double total = taxableAmount + gst + pst;
 
+        System.out.println("Subtotal: " + convToCAD(subtotal));
+        System.out.println("Loyalty discount: " + convToCAD(discount));
+        System.out.println("Taxable amount: " + convToCAD(taxableAmount));
+        System.out.println("GST: " + convToCAD(gst));
+        System.out.println("PST: " + convToCAD(pst));
+        System.out.println("Total: " + convToCAD(total));
+
+
+    }
+
+    private static String convToCAD(double n) {
+        String s = "";
+        double abs = n;
+        if (n < 0) {
+            s += '-';
+            abs *= -1;
+        }
+        abs = Math.round(abs*100.0)/100.0;
+        s += "$"+abs+" CAD";
+        return s;
     }
 }
